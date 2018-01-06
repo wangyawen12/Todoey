3418ec9b2fa53f6164f3193425d8cad0bcb2705e
Yawen Wang <yawenwang@yawens-mbp>
2018-01-06 12:41:28 -0600

    data persisted using UserDefaults method

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,8 +12,14 @@ class TodoListViewController: UITableViewController{
 
     var itemArray = ["fruit and vegetables", "cosmetics","study stuff"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        if let items = defaults.array(forKey: "TodoListArray") as?[String]{
+            itemArray = items
+        }
         
     }
     //MARK - Tableview Datasource Methods
@@ -65,6 +71,8 @@ class TodoListViewController: UITableViewController{
     let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
        
         self.itemArray.append(textField.text!)
+        
+        self.defaults.set(self.itemArray, forKey: "TodoListArray")
         
         self.tableView.reloadData()
         
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Yawen Wang on 1/6/18.
+//  Copyright © 2018 Yawen Wang. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    
+    var title :String = ""
+    
+    var done: Bool =  false
+}
