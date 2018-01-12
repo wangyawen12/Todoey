692a85d3ba7fe8e749dbc0fd24b2999282ef6cd9
Yawen Wang <yawenwang@yawens-mbp>
2018-01-11 23:24:31 -0600

    Real for Data Persistence

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -16,7 +16,10 @@ class Item: Object{
     @objc dynamic var done : Bool = false
     @objc dynamic var dateCreated: Date?
     
-    var parentCategory = LinkingObjects(fromType: Category.self, property: "Items")
+    
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+    
+    //var parentCategory = LinkingObjects(fromType: Category.self, property: "Items")
     
    // var parentCategory = LinkingObjects(fromType: Category.self, property:"Items")
 }
